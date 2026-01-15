316049f7dc676a8e22b610e55c5517124cf94649
Rae Allen Tura <rntura@student.apc.edu.ph>
2026-01-15 06:30:39 +0000

    edit

---
+++ firstsfc/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello WEBPROG IT2 foUr threeee! BANGGGGGGGGG!! '
+    };
+  }
+};
+</script>
+
+<style></style>
